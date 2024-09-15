3ae17b3f2f363de01f9e35f5354a95b5e8af1e45
Oleg-git-cketch <aliol439003@gmail.com>
2024-09-15 15:16:32 +0500

    Delete Домашка 1.py

--- Домашка 1.py
+++
@@ -1,8 +0,0 @@
-def summa(a, b):
-    return a + b
-
-while True:
-    numbers = int(input('Введите первое число для сложения: '))
-    numbers2 = int(input('Введите второе число для сложения: '))
-    result = summa(numbers, numbers2)
-    print(f'Результат сложения: {result}')
