42b6b03b83bcccd35270eccb1e53f9115a8ddf8f
Oleg-git-cketch <aliol439003@gmail.com>
2024-09-15 15:15:47 +0500

    Delete дз1.py

--- дз1.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-name_person = Person('Павел', 20)
-
-print(name_person.name)
-print(name_person.age)
